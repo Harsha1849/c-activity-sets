b72678e78945feee8fedab08c1408542e06010ca
harsha <harshaa.bsc23@rvu.edu.in>
2024-01-31 19:48:22 +0530

    test

---
+++ set01/problem09.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+float input();
+float square_root(float n);
+void output(float n, float sqrroot);
+
+int main()
+{
+    float number, sqrt;
+    number=input();
+    sqrt=square_root(number);
+    output(number,sqrt);
+    return 0;
+}
+float input()
+{
+    float n;
+    printf("enter a number:");
+    scanf("%f",&n);
+    return n;
+}
+float square_root(float n)
+{
+    float x=n;
+    float y=1.0;
+    float epsilon=0.00001;
+    while(x-y > epsilon)
+    {
+        x=(x+y)/2;
+        y=n/x;
+    }
+    return x;
+}
+void output(float n, float sqrroot)
+{
+    printf("square root of %.1f is %.1f",n,sqrroot);
+}
