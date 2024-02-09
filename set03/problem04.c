37f28c296c1812ad170c27296e79983021e462ec
harsha <harshaa.bsc23@rvu.edu.in>
2024-02-09 17:13:24 +0530

    test4

---
+++ set03/problem04.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int input();
+int find_fibo(int n);
+void output(int n, int fibo);
+
+int main()
+{
+    int n,fibo;
+    n=input();
+    fibo=find_fibo(n);
+    output(n,fibo);
+    return 0;
+}
+
+int input()
+{
+    int n;
+    printf("enter the value of n to find nth number in fibo");
+    scanf("%d",&n);
+    return n;
+}
+int find_fibo(int n)
+{
+    if(n<=1)
+    return n;
+    int a=0,b=1,fibo;
+    for (int i=2;i<=n;i++)
+    {
+        fibo=a+b;
+        a=b;
+        b=fibo;
+    }
+    return fibo;
+}
+void output(int n, int fibo)
+{
+    printf("the %dth number in fivo is: %d\n",n,fibo);
+}    
