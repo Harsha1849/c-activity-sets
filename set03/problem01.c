bcb3f940407f8dd622774628889a0af2ca79e0d5
harsha <harshaa.bsc23@rvu.edu.in>
2024-02-29 18:16:48 +0530

    test01

--- set03/problem01.c
+++ set03/problem01.c
@@ -9,7 +9,7 @@ void input(float *x1, float *y1, float *x2, float *y2)
 }
 float find_distance(float x1,float y1, float x2, float y2)
 {
-    float distance=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    float distance=sqrt(pow(x2-x1, 2)+pow(y2-y1, 2));
     return distance;
 }
 
